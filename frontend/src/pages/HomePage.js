07b24114e68d7a3da0eb5c634b62c61179c65285
[Mario] <[meins2320@gmail.com]>
2021-06-16 12:18:29 +0200

    add dropdown in searchpage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom";
+import {Button} from "@material-ui/core";
 
 export default function HomePage(){
 
@@ -9,9 +10,7 @@ export default function HomePage(){
                 Wind und Wellen
             </p>
             <div>
-                <Link to={"/search"}>
-                    <button component={Link} >Look for Spots</button>
-                </Link>
+                <Button variant="outlined" component={Link} to={"/search"}>Look for Spots here</Button>
             </div>
 
         </div>
--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,11 +1,48 @@
 import SpotItem from "../component/SpotItem";
 import styled from "styled-components/macro";
+import {FormControl, InputLabel, makeStyles, Select} from "@material-ui/core";
+import React from 'react';
+
+
+
 
 export default function SearchPage({spots}){
 
 
+
+        const classes = useStyles();
+        const [state, setState] = React.useState({
+            age: '',
+            name: 'hai',
+        });
+
+        const handleChange = (event) => {
+            const name = event.target.name;
+            setState({
+                ...state,
+                [name]: event.target.value,
+            });
+        };
+
     return(
         <Wrapper>
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="age-native-simple">Country</InputLabel>
+                <Select
+                    native
+                    value={state.age}
+                    onChange={handleChange}
+                    inputProps={{
+                        name: 'age',
+                        id: 'age-native-simple',
+                    }}
+                >
+                    <option aria-label="None" value="" />
+                    <option value={10}>France</option>
+                    <option value={20}>Portugal</option>
+                    <option value={30}>Spain</option>
+                </Select>
+            </FormControl>
             <ul>
                 {spots.map((spot) => (
                     <li>
@@ -28,3 +65,13 @@ const Wrapper = styled.div`
     border: black solid 1px;
   }
 `
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
