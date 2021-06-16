6abbe8f9128a1661fdfca1a6f99b148ca1c39b61
[Mario] <[meins2320@gmail.com]>
2021-06-16 09:53:17 +0200

    add spots hard coded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,36 @@ import {Switch, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 function App() {
+
+    const spots = [
+        {
+        "name" : "Spot1",
+        "geograhy" : {
+            "longitude" : 1,
+            "latidude" : 1,
+            "continent" : "Europa",
+            "country" : "France",
+            "Region" : "Basque",
+        },
+        "break" : "Beach",
+        "level" : "All",
+
+
+    },
+        {
+        "name" : "Spot2",
+        "geograhy" : {
+            "longitude" : 2,
+            "latidude" : 1,
+            "continent" : "Europa",
+            "country" : "France",
+            "Region" : "Bretagne",
+        },
+        "break" : "Beach and Reef",
+        "level" : "All",
+
+        }]
+
   return (
       <Switch>
         <div>
--- frontend/src/component/SpotItem.js
+++ frontend/src/component/SpotItem.js
@@ -1,4 +1,4 @@
-export default function ListItem(){
+export default function SpotItem(){
     return(
         <div>
             <p>
