2266f3905146633569faef4fc79b7f30185fa82a
[Mario] <[meins2320@gmail.com]>
2021-06-28 15:08:18 +0200

    some design

--- frontend/src/component/DetailsHeader.js
+++ frontend/src/component/DetailsHeader.js
@@ -1,61 +1,110 @@
 import React from "react"
 import {rightTimeToShowCurrentTemp} from "../service/surfSpotCalculationService";
+import styled from "styled-components/macro";
+const Wrapper = styled.section`
+  border-bottom: black solid 2px;
+  background-color: aquamarine;
+  border-top-right-radius: 2%;
+  border-top-left-radius: 2%;
+  
+  .title{
+    padding-top: 2px;
+    
+  }
+  
+  h2{
+    text-align: center;
+    padding-bottom: 0px;
+    margin-bottom: 0px;
+  }
+  
+  .subtitle{
+    display: flex;
+    justify-content: space-between ;
+    font-size: 12px;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .body{
+    display: flex;
+   justify-content: space-between;
+    
+  }
+  
+  .body-left{
+    display: flex;
+    flex-flow: column ;
+    font-size: 14px;
+  }
+  
+  .body-right{
+    display: flex;
+    flex-flow: column ;
+    font-size: 14px;
+    
+  }
+  .body.p{
+    padding: 0px;
+    margin: 0px;
+  }
+`
+
+
 export default function DetailsHeader({surfSpot, currentSurfData}){
 
     const now = Date.now()
-    //console.log(currentSurfData)
+
     return(
-        <section>
-            <div>
-                {surfSpot?.spotLocationDetails.name}
-                {surfSpot?.spotLocationDetails.continent}
-                {surfSpot?.spotLocationDetails.country}
-                {surfSpot?.spotLocationDetails.region}
-            </div>
-            <div>
-                {new Date(now).toUTCString()}
-            </div>
-            <div>
-                <p>
-                    Ait Temperature: {currentSurfData?.airTemperature.sg}
-                </p>
+        <Wrapper>
+            <div className="title">
+                <h2>{surfSpot?.spotLocationDetails.name}</h2>
+                <div className="subtitle">
+                    <p>{surfSpot?.spotLocationDetails.continent}, {surfSpot?.spotLocationDetails.country}, {surfSpot?.spotLocationDetails.region}</p>
+                    <p>{new Date(now).toUTCString()}</p>
+                </div>
 
             </div>
-            <div>
-                <p>
-                    Water Temperature: {currentSurfData?.waterTemperature.sg}
-                </p>
-
-            </div>
-            <div>
-                <p>
-                    Wave Direction:  {currentSurfData?.swellDirection.sg}
-                </p>
-
-            </div>
-            <div>
-                <p>
-                    Wave Height: {currentSurfData?.swellHeight.sg}
-                </p>
-
-            </div>
-            <div>
-                <p>
-                    Wave Period: {currentSurfData?.swellPeriod.sg}
-                </p>
-
-            </div>
-            <div>
-                <p>
-                    Wind Direction: {currentSurfData?.windDirection.sg}
-                </p>
-
-            </div>
-            <div>
-                <p>
-                    Wind Speed: {currentSurfData?.windSpeed.sg}
-                </p>
-            </div>
-        </section>
+            <section className="body">
+                <div className="body-left">
+                    <div>
+                        <p>
+                            Air: {currentSurfData?.airTemperature.sg}
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Water: {currentSurfData?.waterTemperature.sg}
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Wave Direction:  {currentSurfData?.swellDirection.sg}
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Wave Height: {currentSurfData?.swellHeight.sg}
+                        </p>
+                    </div>
+                </div>
+                <div className="body-right">
+                    <div>
+                        <p>
+                            Wave Period: {currentSurfData?.swellPeriod.sg}
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Wind Direction: {currentSurfData?.windDirection.sg}
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            Wind Speed: {currentSurfData?.windSpeed.sg}
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </Wrapper>
     )
 }
